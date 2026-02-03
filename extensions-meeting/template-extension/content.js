27d63985070c6488ad65d165751042390f2b84bc
PBHS-CS-Club <maurizio.stradiotti01+csclub@gmail.com>
2026-02-03 18:19:51 -0500

    Create content.js

---
+++ extensions-meeting/template-extension/content.js
@@ -0,0 +1,3 @@
+// your code here
+// you can get an array of every element by using the code: Array.from(document.querySelectorAll('*'));
+// and you can change background color by using the code: [replace with your element].style.backgroundColor = "black";
